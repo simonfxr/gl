adf60402225992549894cf343a6f1fff71fbb637
Simon Reiser <simonfxr@googlemail.com>
2013-07-06 21:26:59 +0200

    up

--- programs/springs/shaders/physics.vert
+++ programs/springs/shaders/physics.vert
@@ -1,10 +1,40 @@
 
-in vec4 position_mass;
+uniform samplerBuffer position_mass_sampler;
+uniform float dt;
+uniform float D;
+uniform float damping;
 
-out Vertex {
+in vec4 position_mass;
+in vec3 velocity;
+in ivec4 connection;
+
+out Out1 {
     vec4 position_mass;
-};
+} out1;
+
+out Out2 {
+    vec3 velocity;
+} out2;
 
 void main() {
-    Vertex.position_mass = position_mass;
+
+    vec3 pos = position_mass.xyz;
+    float inv_mass = position_mass.w;
+    vec3 v = velocity;
+
+    vec3 F = vec3(0);
+
+    for (int i = 0; i < 4; ++i) {
+        vec3 r = texelFetch(position_mass_sampler, connection[i]).xyz;
+        vec3 s = r - pos;
+        F += D * (length(s) - 1.0) * normalize(s);
+    }
+
+    vec3 a = inv_mass == 0 ? vec3(0) : F * inv_mass + vec3(0, -9.81, 0);
+
+    pos += dt * v + (0.5 * dt * dt) * a;
+    v += dt * damping * a;
+    
+    out1.position_mass = vec4(pos, inv_mass);
+    out2.velocity = v;
 }
--- programs/springs/springs.cpp
+++ programs/springs/springs.cpp
@@ -8,7 +8,7 @@
 #include "glt/utils.hpp"
 #include "glt/Uniforms.hpp"
 
-#include "data/Array.hpp"
+#include "data/SharedArray.hpp"
 
 #include "math/mat3.hpp"
 #include "math/mat4.hpp"
@@ -46,17 +46,20 @@ struct Anim {
 
     glt::Mesh<Vertex> sphere_model;
 
-    GLuint particle_vertex_array;
+    GLuint particle_vertex_array[2];
 
     index particle_source; // 0 or 1
     
-    // vec4 of (x, y, z, m)
-    GLuint particle_data1_handle[2];
+    // vec4 of (x, y, z, 1/m)
+    GLuint particle_pos_mass_handle[2];
+    // velocity (vec3)
+    GLuint particle_vel_handle[2];
     // ivec4 spring connections
-    GLuint particle_data2_handle;
+    GLuint particle_conn_handle;
 
-    GLuint particle_data1_tex[2];
-    GLuint particle_data2_tex;
+    GLuint particle_pos_mass_tex[2];
+    GLuint particle_vel_tex[2];
+    GLuint particle_conn_tex;
 
     Ref<glt::ShaderProgram> physics_prog;
     
@@ -73,72 +76,102 @@ void Anim::init(const ge::Event<ge::InitEvent>& ev) {
     engine.enablePlugin(camera);
     camera.frame().origin = vec3(0.f);
 
+    engine.gameLoop().ticksPerSecond(400);
+
     glt::primitives::sphere(sphere_model, 1.f, 30, 15);
     sphere_model.send();
     sphere_model.enableAttribute(1, false); // disable normals
 
-    GL_CALL(glGenVertexArrays, 1, &particle_vertex_array);
-    GL_CALL(glGenBuffers, 2, particle_data1_handle);
-    GL_CALL(glGenBuffers, 1, &particle_data2_handle);
-
     particle_source = 0;
-    std::vector<vec4_t> particle_data1;
-    std::vector<GLshort>  particle_data2;
-
+    std::vector<vec4_t> particle_pos_mass;
+    std::vector<vec3_t> particle_vel;
+    std::vector<GLint>  particle_conn;
 
     for (index i = 0; i < N_SPRINGS; ++i) {
         for (index j = 0; j < N_SPRINGS; ++j) {
             // arrange in grid of N_SPRINGS by N_SPRINGS 
             vec3_t pos = vec3(real(i * N_SPRINGS), 0, real(j * N_SPRINGS));
-            real mass = real(1) / real(N_SPRINGS * N_SPRINGS); // accumulated
-                                                     // mass is 1
-            particle_data1.push_back(vec4(pos[0], pos[1], pos[2], mass));
+            real inv_mass = real(N_SPRINGS * N_SPRINGS); // accumulated
+                                                         // mass is 1
+            if ((i == 0 || i == N_SPRINGS - 1) || (j == 0 || j == N_SPRINGS - 1)) {
+                inv_mass = 0.f; // the edges have infinite mass;
+            }
+
+            
+            particle_pos_mass.push_back(vec4(pos[0], pos[1], pos[2], inv_mass));
+            particle_vel.push_back(vec3(real(0)));
 #define INDEX(i, j) ((i) * N_SPRINGS + (j))
-            particle_data2.push_back(i > 0 ? INDEX(i - 1, j) : -1);
-            particle_data2.push_back(i + 1 < N_SPRINGS ? INDEX(i + 1, j) : -1);
-            particle_data2.push_back(j > 0 ? INDEX(i, j - 1) : -1);
-            particle_data2.push_back(j + 1 < N_SPRINGS ? INDEX(i, j + 1) : -1);
+            particle_conn.push_back(i > 0 ? INDEX(i - 1, j) : INDEX(i, j));
+            particle_conn.push_back(i + 1 < N_SPRINGS ? INDEX(i + 1, j) : INDEX(i, j));
+            particle_conn.push_back(j > 0 ? INDEX(i, j - 1) : INDEX(i, j));
+            particle_conn.push_back(j + 1 < N_SPRINGS ? INDEX(i, j + 1) : INDEX(i, j));
 #undef INDEX
         }
     }
 
-    GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, particle_data1_handle[0]);
-    GL_CALL(glBufferData, GL_ARRAY_BUFFER, sizeof(vec4_t) * particle_data1.size(), &particle_data1[0], GL_STREAM_DRAW);
-    GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, particle_data2_handle);
-    GL_CALL(glBufferData, GL_ARRAY_BUFFER, sizeof(GLshort) * particle_data1.size(), &particle_data2[0], GL_STREAM_DRAW);
+    GL_CALL(glGenVertexArrays, 2, particle_vertex_array);
+    GL_CALL(glGenBuffers, 2, particle_pos_mass_handle);
+    GL_CALL(glGenBuffers, 2, particle_vel_handle);
+    GL_CALL(glGenBuffers, 1, &particle_conn_handle);
 
-    GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, particle_data1_handle[1]);
-    GL_CALL(glBufferData, GL_ARRAY_BUFFER, sizeof(vec4_t) * particle_data1.size(), 0, GL_STREAM_DRAW);
+    GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, particle_pos_mass_handle[0]);
+    GL_CALL(glBufferData, GL_ARRAY_BUFFER, sizeof(vec4_t) * particle_pos_mass.size(), &particle_pos_mass[0], GL_STREAM_DRAW);
 
-    // GL_CALL(glBindVertexArray, particle_vertex_array);
-
-    // GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, particle_data1_handle);
-    // GL_CALL(glVertexAttribPointer, 0, 4, GL_FLOAT, GL_FALSE, 0, 0);
-    // GL_CALL(glEnableVertexAttribArray, 0);
+    GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, particle_vel_handle[0]);
+    GL_CALL(glBufferData, GL_ARRAY_BUFFER, sizeof(vec3_t) * particle_vel.size(), &particle_vel[0], GL_STREAM_DRAW);
     
-    // GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, particle_data2_handle);
-    // GL_CALL(glVertexAttribIPointer, 1, 4, GL_SHORT, 0, 0);
-    // GL_CALL(glEnableVertexAttribArray, 1);
-            
-    // GL_CALL(glBindVertexArray, 0);
+    GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, particle_pos_mass_handle[1]);
+    GL_CALL(glBufferData, GL_ARRAY_BUFFER, sizeof(vec4_t) * particle_pos_mass.size(), 0, GL_STREAM_DRAW);
 
-    GL_CALL(glGenTextures, 2, particle_data1_tex);
-    GL_CALL(glGenTextures, 1, &particle_data2_tex);
+    GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, particle_vel_handle[1]);
+    GL_CALL(glBufferData, GL_ARRAY_BUFFER, sizeof(vec3_t) * particle_vel.size(), 0, GL_STREAM_DRAW);
+
+    GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, particle_conn_handle);
+    GL_CALL(glBufferData, GL_ARRAY_BUFFER, sizeof(GLint) * particle_conn.size(), &particle_conn[0], GL_STREAM_DRAW);
 
     for (index i = 0; i < 2; ++i) {
-        GL_CALL(glBindTexture, GL_TEXTURE_BUFFER, particle_data1_tex[i]);
-        GL_CALL(glTexBuffer, GL_TEXTURE_BUFFER, GL_RGBA32F, particle_data1_handle[i]);
+        GL_CALL(glBindVertexArray, particle_vertex_array[i]);
+
+        GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, particle_pos_mass_handle[i]);
+        GL_CALL(glVertexAttribPointer, 0, 4, GL_FLOAT, GL_FALSE, 0, 0);
+        GL_CALL(glEnableVertexAttribArray, 0);
+
+        GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, particle_vel_handle[i]);
+        GL_CALL(glVertexAttribPointer, 1, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        GL_CALL(glEnableVertexAttribArray, 1);
+
+        GL_CALL(glBindBuffer, GL_ARRAY_BUFFER, particle_conn_handle);
+        GL_CALL(glVertexAttribIPointer, 2, 4, GL_INT, 0, 0);
+        GL_CALL(glEnableVertexAttribArray, 2);
+            
+        GL_CALL(glBindVertexArray, 0);
+    }
+
+    GL_CALL(glGenTextures, 2, particle_pos_mass_tex);
+    GL_CALL(glGenTextures, 2, particle_vel_tex);
+    GL_CALL(glGenTextures, 1, &particle_conn_tex);
+
+    for (index i = 0; i < 2; ++i) {
+        GL_CALL(glBindTexture, GL_TEXTURE_BUFFER, particle_pos_mass_tex[i]);
+        GL_CALL(glTexBuffer, GL_TEXTURE_BUFFER, GL_RGBA32F, particle_pos_mass_handle[i]);
+
+        GL_CALL(glBindTexture, GL_TEXTURE_BUFFER, particle_vel_tex[i]);
+        GL_CALL(glTexBuffer, GL_TEXTURE_BUFFER, GL_RGB32F, particle_vel_handle[i]);
     }
     
-    GL_CALL(glBindTexture, GL_TEXTURE_BUFFER, particle_data2_tex);
-    GL_CALL(glTexBuffer, GL_TEXTURE_BUFFER, GL_RGBA16I, particle_data2_handle);
+    GL_CALL(glBindTexture, GL_TEXTURE_BUFFER, particle_conn_tex);
+    GL_CALL(glTexBuffer, GL_TEXTURE_BUFFER, GL_RGBA32I, particle_conn_handle);
 
     physics_prog = makeRef(new glt::ShaderProgram(engine.shaderManager()));
     physics_prog->addShaderFile("physics.vert");
     physics_prog->bindAttribute("position_mass", 0);
-    std::string stream_vars[] = { "Vertex.position_mass" };
-    physics_prog->bindStreamOutVaryings(SharedArray<std::string>(stream_vars, ARRAY_LENGTH(stream_vars)));
-    physics_prog->link();
+    physics_prog->bindAttribute("velocity", 1);
+    physics_prog->bindAttribute("connection", 2);
+    const char *stream_vars[] = { "Out1.position_mass", "Out2.velocity" };
+
+    GL_CALL(glTransformFeedbackVaryings, *physics_prog->program(), ARRAY_LENGTH(stream_vars), stream_vars, GL_SEPARATE_ATTRIBS);
+    
+    ASSERT(physics_prog->link());
     
     ev.info.success = true;
 }
@@ -157,7 +190,7 @@ void Anim::renderScene(const ge::Event<ge::RenderEvent>&) {
     ASSERT(prog);
 
     GL_CALL(glActiveTexture, GL_TEXTURE0);
-    GL_CALL(glBindTexture, GL_TEXTURE_BUFFER, particle_data1_tex[particle_source]);
+    GL_CALL(glBindTexture, GL_TEXTURE_BUFFER, particle_pos_mass_tex[particle_source]);
 
     prog->use();
     glt::Uniforms(*prog)
@@ -169,7 +202,34 @@ void Anim::renderScene(const ge::Event<ge::RenderEvent>&) {
 }
 
 void Anim::animate(const ge::Event<ge::AnimationEvent>&) {
+    GL_CALL(glEnable, GL_RASTERIZER_DISCARD);
 
+    index particle_dest = (1 + particle_source) % 2;
+
+    physics_prog->use();
+
+    GL_CALL(glActiveTexture, GL_TEXTURE0);
+    GL_CALL(glBindTexture, GL_TEXTURE_BUFFER, particle_pos_mass_tex[particle_source]);
+
+
+    glt::Uniforms(*physics_prog)
+        .optional("dt", engine.gameLoop().frameDuration())
+        .optional("D", 0.1f)
+        .optional("damping", 0.98f)
+        .mandatory("position_mass_sampler", glt::BoundTexture(GL_SAMPLER_BUFFER, 0));
+    
+    GL_CALL(glBindBufferBase, GL_TRANSFORM_FEEDBACK_BUFFER, 0, particle_pos_mass_handle[particle_dest]);
+    GL_CALL(glBindBufferBase, GL_TRANSFORM_FEEDBACK_BUFFER, 1, particle_vel_handle[particle_dest]);
+
+    GL_CALL(glBindVertexArray, particle_vertex_array[particle_source]);
+
+    GL_CALL(glBeginTransformFeedback, GL_POINTS);
+    GL_CALL(glDrawArrays, GL_POINTS, 0, N_SPRINGS * N_SPRINGS);
+    GL_CALL(glEndTransformFeedback);
+    
+    GL_CALL(glDisable, GL_RASTERIZER_DISCARD);
+
+    particle_source = particle_dest;
 }
 
 int main(int argc, char *argv[]) {
