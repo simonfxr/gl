9dc8233a63b9224ab9adb4d56e5f020c0b19aced
Simon Reiser <simonfxr@googlemail.com>
2014-01-05 21:15:32 +0100

    up

--- programs/mc/cl/program.cl
+++ programs/mc/cl/program.cl
@@ -1,3 +1,4 @@
+#pragma OPENCL EXTENSION cl_khr_3d_image_writes : enable
 
 constant sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP | CLK_FILTER_NEAREST;
 
@@ -57,7 +58,27 @@ __constant int4 cube_offsets[8] = {
     {1, 1, 0, 0},
     {0, 1, 1, 0},
     {1, 1, 1, 0},
-}; 
+};
+
+__kernel void computeHistogramLevel(
+    __read_only image3d_t source,
+    __write_only image3d_t dest)
+{
+    int4 wpos = { get_global_id(0), get_global_id(1), get_global_id(2), 0 };
+    int4 rpos = wpos * 2;
+
+    int count = 0;
+    count += read_imagei(source, sampler, rpos + cube_offsets[0]).x;
+    count += read_imagei(source, sampler, rpos + cube_offsets[1]).x;
+    count += read_imagei(source, sampler, rpos + cube_offsets[2]).x;
+    count += read_imagei(source, sampler, rpos + cube_offsets[3]).x;
+    count += read_imagei(source, sampler, rpos + cube_offsets[4]).x;
+    count += read_imagei(source, sampler, rpos + cube_offsets[5]).x;
+    count += read_imagei(source, sampler, rpos + cube_offsets[6]).x;
+    count += read_imagei(source, sampler, rpos + cube_offsets[7]).x;
+
+    write_imagei(dest, wpos, count);
+}
 
 __kernel void computeHistogramBaseLevel(
     __write_only image3d_t histo_base,
@@ -129,4 +150,145 @@ __kernel void generateSphereVolume(__write_only image3d_t data,
 
     pos.s3 = 0;
     write_imagei(data, pos, (uchar) (int) value);
+}
+
+int4 scanHistogramLevel(int index, __read_only image3d_t histo, int4 pos) {
+
+    int8 neighbors = {
+        read_imagei(histo, sampler, pos + cube_offsets[0]).x,
+        read_imagei(histo, sampler, pos + cube_offsets[1]).x,
+        read_imagei(histo, sampler, pos + cube_offsets[2]).x,
+        read_imagei(histo, sampler, pos + cube_offsets[3]).x,
+        read_imagei(histo, sampler, pos + cube_offsets[4]).x,
+        read_imagei(histo, sampler, pos + cube_offsets[5]).x,
+        read_imagei(histo, sampler, pos + cube_offsets[6]).x,
+        read_imagei(histo, sampler, pos + cube_offsets[7]).x
+    };
+    
+    int acc = pos.s3;
+    int8 cmp;
+
+    acc += neighbors.s0;
+    cmp.s0 = acc <= index;
+    acc += neighbors.s1;
+    cmp.s1 = acc <= index;
+    acc += neighbors.s2;
+    cmp.s2 = acc <= index;
+    acc += neighbors.s3;
+    cmp.s3 = acc <= index;
+    acc += neighbors.s4;
+    cmp.s4 = acc <= index;
+    acc += neighbors.s5;
+    cmp.s5 = acc <= index;
+    acc += neighbors.s6;
+    cmp.s6 = acc <= index;
+    cmp.s7 = 0;
+
+    pos += cube_offsets[cmp.s0 + cmp.s1 + cmp.s2 + cmp.s3 +
+                        cmp.s4 + cmp.s5 + cmp.s6 + cmp.s7];
+
+    pos.s0 *= 2;
+    pos.s1 *= 2;
+    pos.s2 *= 2;
+    
+    neighbors *= cmp;
+    pos.s3 +=
+        neighbors.s0 + neighbors.s1 + neighbors.s2 + neighbors.s3 +
+        neighbors.s4 + neighbors.s5 + neighbors.s6 + neighbors.s7;
+    
+    return pos;
+}
+
+float3 calculateGradient(__read_only image3d_t h0, int4 p) {
+    int dx = read_imagei(h0, sampler, p + (int4) { 1, 0, 0, 0 }).s2 -
+        read_imagei(h0, sampler, (int4) { -1, 0, 0, 0 }).s2;
+    int dy = read_imagei(h0, sampler, p + (int4) { 0, 1, 0, 0 }).s2 -
+        read_imagei(h0, sampler, (int4) { 0, -1, 0, 0 }).s2;
+    int dz = read_imagei(h0, sampler, p + (int4) { 0, 0, 1, 0 }).s2 -
+        read_imagei(h0, sampler, (int4) { 0, 0, -1, 0 }).s2;
+
+    // should be halfed, but doesnt matter, will be normalized anyway
+    return convert_float3((int3) { dx, dy, dz });
+}
+
+float3 normalize3(float3 a) {
+    float il = rsqrt(a.x * a.x + a.y * a.y + a.z * a.z);
+    return a * il;
+}
+
+__kernel void histogramTraversal(
+    __read_only image3d_t h0,
+    __read_only image3d_t h1,
+    __read_only image3d_t h2,
+       __read_only image3d_t h3,
+    __read_only image3d_t h4,
+    __read_only image3d_t h5,
+    #if N > 64
+    __read_only image3d_t h6,
+    #endif
+    #if N > 128
+    __read_only image3d_t h7,
+    #endif
+    #if N > 256
+    __read_only image3d_t h8,
+    #endif
+    __global float *vbo,
+    __private int isolevel,
+    __private int num_tris)
+{
+
+    int index = get_global_id(0);
+    if (index >= num_tris)
+        index = 0;
+
+    int4 cube_position = (int4) 0;
+
+    #if N > 256
+    cube_position = scanHistogramLevel(index, h8, cube_position);
+    #endif
+    #if N > 128
+    cube_position = scanHistogramLevel(index, h7, cube_position);
+    #endif
+    #if N > 64
+    cube_position = scanHistogramLevel(index, h6, cube_position);
+    #endif
+    cube_position = scanHistogramLevel(index, h5, cube_position);
+    cube_position = scanHistogramLevel(index, h4, cube_position);
+    cube_position = scanHistogramLevel(index, h3, cube_position);
+    cube_position = scanHistogramLevel(index, h2, cube_position);
+    cube_position = scanHistogramLevel(index, h1, cube_position);
+    cube_position = scanHistogramLevel(index, h0, cube_position);
+
+    cube_position.s0 /= 2;
+    cube_position.s1 /= 2;
+    cube_position.s2 /= 2;
+
+    const int4 cube_data = read_imagei(h0, sampler, cube_position);
+
+    int vertex_index = index * 6;
+    for (int i = (index - cube_position.s3) * 3; i < (index - cube_position.s3 + 1) * 3; ++i) {
+        const uchar edge = triangle_edge_table[cube_data.s1 * 16 + i];
+        const int4 p0 = (int4)(cube_position.x + offsets3[edge * 6 + 0],
+                               cube_position.y + offsets3[edge * 6 + 1],
+                               cube_position.z + offsets3[edge * 6 + 2], 0);
+        
+        const int4 p1 = (int4)(cube_position.x + offsets3[edge * 6 + 3],
+                               cube_position.y + offsets3[edge * 6 + 4],
+                               cube_position.z + offsets3[edge * 6 + 5], 0);
+
+        float3 n0 = normalize3(- calculateGradient(h0, p0));
+        float3 n1 = normalize3(- calculateGradient(h0, p1));
+
+        int v0 = read_imagei(h0, sampler, p0).s2;
+        int v1 = read_imagei(h0, sampler, p1).s2;
+        const float t = native_divide((float)(isolevel - v0), (float)(v1 - v0));
+
+        float3 pos = mix(convert_float3(p0.xyz), convert_float3(p1.xyz), t);
+        float3 normal = mix(n0, n1, t);
+
+        vstore3(pos, vertex_index * 2, vbo);
+        vstore3(normal, vertex_index * 2 + 1, vbo);
+        ++vertex_index;
+    }
+
 }
--- programs/mc/mc.cpp
+++ programs/mc/mc.cpp
@@ -11,6 +11,8 @@
 #include "glt/utils.hpp"
 #include "glt/GLSLPreprocessor.hpp"
 
+#include "sys/measure.hpp"
+
 #include "shaders/shader_constants.h"
 
 #include "ge/Engine.hpp"
@@ -26,7 +28,9 @@
 #include <GL/glx.h>
 #endif
 
-static const size N = 256;
+#define CL_ERR(msg) do { if (cl_err != CL_SUCCESS) { ERR(msg); return; } } while (0)
+
+static const size DEFAULT_N = 256;
 
 using namespace math;
 
@@ -40,19 +44,29 @@ DEFINE_VERTEX_DESC(Vertex,
 struct Anim {
     ge::Engine engine;
     real time_print_fps;
-    
+
+    size N;
     cl::Context cl_ctx;
     cl::CommandQueue cl_q;
+    cl::Device cl_device;
 
     cl::Program cl_program;
     cl::Kernel kernel_generateSphereVolume;
     cl::Kernel kernel_computeHistogramBaseLevel;
+    cl::Kernel kernel_computeHistogramLevel;
+    cl::Kernel kernel_histogramTraversal;
 
     cl::Image3D volumeData;
     std::vector<cl::Image3D> images;
     
     void init(const ge::Event<ge::InitEvent>&);
     void initCL(const ge::Event<ge::InitEvent>&);
+    void initCLKernels(bool *success);
+    void initCLData(bool *success);
+
+    size computeHistogram();
+    void constructVertices(size num_tris);
+    
     void link(ge::Engine& e);
     void animate(const ge::Event<ge::AnimationEvent>&);
     void renderScene(const ge::Event<ge::RenderEvent>&);
@@ -97,7 +111,22 @@ static cl::Context createCLGLContext(cl::Platform& platform, cl_int *err) {
     return cl::Context(CL_DEVICE_TYPE_GPU, props, print_cl_error, NULL, err);
 }
 
+template <typename T>
+T log2(T x) {
+    T l = 1;
+    T i = 0;
+    while (l < x) {
+        l *= 2;
+        ++i;
+    }
+
+    return i;
+}
+
 void Anim::initCL(const ge::Event<ge::InitEvent>& ev) {
+
+    N = DEFAULT_N;
+    
     std::vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
 
@@ -129,9 +158,9 @@ void Anim::initCL(const ge::Event<ge::InitEvent>& ev) {
 
     engine.out() << "  END EXTENSIONS" << sys::io::endl;
 
-    cl_int err;
-    cl_ctx = createCLGLContext(platform, &err);
-    if (err != CL_SUCCESS) {
+    cl_int cl_err;
+    cl_ctx = createCLGLContext(platform, &cl_err);
+    if (cl_err != CL_SUCCESS) {
         ERR("createing shared OpenCL/OpenGL context failed");
         return;
     }
@@ -143,10 +172,10 @@ void Anim::initCL(const ge::Event<ge::InitEvent>& ev) {
         return;
     devices.resize(1);
     engine.out() << "selecting CL device:" << sys::io::endl;
-    cl::Device& dev = devices[0];
+    cl_device = devices[0];
 
 #define DEV_INFO(info)                                                  \
-    dev.getInfo(info, &info_value);                                     \
+    cl_device.getInfo(info, &info_value);                               \
     engine.out() << "  " << #info << ": " << info_value << sys::io::endl
 
     DEV_INFO(CL_DEVICE_NAME);
@@ -157,7 +186,7 @@ void Anim::initCL(const ge::Event<ge::InitEvent>& ev) {
     DEV_INFO(CL_DRIVER_VERSION);
 
     engine.out() << "  CL_DEVICE_EXTENSIONS:" << sys::io::endl;
-    dev.getInfo(CL_DEVICE_EXTENSIONS, &info_value);
+    cl_device.getInfo(CL_DEVICE_EXTENSIONS, &info_value);
     std::istringstream dev_exts(info_value);
     std::string dev_ext;
 
@@ -175,27 +204,73 @@ void Anim::initCL(const ge::Event<ge::InitEvent>& ev) {
         return;
     }
 
-    cl_q = cl::CommandQueue(cl_ctx, dev, 0, &err);
-    if (err != CL_SUCCESS) {
-        ERR("creating command queue failed");
+    cl_q = cl::CommandQueue(cl_ctx, cl_device, 0, &cl_err);
+    CL_ERR("creating command queue failed");
+
+    bool ok = false;
+    initCLKernels(&ok);
+    if (!ok) return;
+    
+    ok = false;
+    initCLData(&ok);
+    if (!ok) return;
+
+    ev.info.success = true;
+}
+
+void Anim::initCLKernels(bool *success) {
+    cl_int cl_err;
+    char *source_code;
+    uint32 code_size;
+
+    if (!glt::readFile(engine.out(), "programs/mc/cl/program.cl", source_code, code_size)) {
+        ERR("failed reading CL program");
         return;
     }
 
-#define IS_POWER_OF_2(x) (((x - 1) & x) == 0)
+    cl::Program::Sources source(1, std::make_pair(source_code, code_size));
+    cl_program = cl::Program(cl_ctx, source, &cl_err);
+    CL_ERR("creating program failed");
 
+    std::stringstream num;
+    num << N;
+    std::vector<cl::Device> devices;
+    devices.push_back(cl_device);
+    cl_err = cl_program.build(devices, (std::string("-I programs/mc/ -DSIZE=") + num.str()).c_str());
+    engine.out() << "building cl program: " << (cl_err == CL_SUCCESS ? "success" : "failed") << ", log:" << sys::io::endl;
+    engine.out() << cl_program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(cl_device) << sys::io::endl;
+
+    if (cl_err != CL_SUCCESS)
+        return;
+
+    kernel_generateSphereVolume = cl::Kernel(cl_program, "generateSphereVolume", &cl_err);
+    CL_ERR("creating kernel failed: generateSphereVolume");
+    kernel_computeHistogramBaseLevel = cl::Kernel(cl_program, "computeHistogramBaseLevel", &cl_err);
+    CL_ERR("creating kernel failed: computeHistogramBaseLevel");
+    kernel_computeHistogramLevel = cl::Kernel(cl_program, "computeHistogramLevel", &cl_err);
+    CL_ERR("creating kernel failed: computeHistogramLevel");
+    kernel_histogramTraversal = cl::Kernel(cl_program, "histogramTraversal", &cl_err);
+    CL_ERR("creating kernel failed: histogramTraversal");
+
+    *success = true;
+}
+
+
+void Anim::initCLData(bool *success) {
+
+#define IS_POWER_OF_2(x) (((x - 1) & x) == 0)
+    int cl_err;
+    
     ASSERT_MSG(IS_POWER_OF_2(N), "N has to be a power of 2!");
     ASSERT_MSG(N <= 256, "N has to be <= 256");        
 
-#define CL_ERR(msg) do { if (err != CL_SUCCESS) { ERR(msg); return; } } while (0)
-
-    volumeData = cl::Image3D(cl_ctx, CL_MEM_READ_WRITE, cl::ImageFormat(CL_R, CL_UNSIGNED_INT8), N, N, N, 0, 0, 0, &err);
+    volumeData = cl::Image3D(cl_ctx, CL_MEM_READ_WRITE, cl::ImageFormat(CL_R, CL_UNSIGNED_INT8), N, N, N, 0, 0, 0, &cl_err);
     CL_ERR("creating volume 3d image failed");
 
-    images.push_back(cl::Image3D(cl_ctx, CL_MEM_READ_WRITE, cl::ImageFormat(CL_RGBA, CL_UNSIGNED_INT8), N, N, N, 0, 0, 0, &err));
+    images.push_back(cl::Image3D(cl_ctx, CL_MEM_READ_WRITE, cl::ImageFormat(CL_RGBA, CL_UNSIGNED_INT8), N, N, N, 0, 0, 0, &cl_err));
     CL_ERR("creating histogram base level failed");
     
     size sz = N / 2;
-    int i = 0;
     int max_size = 5;
 
     // apparently creating a 3d texture of size 1x1x1 fails
@@ -210,45 +285,99 @@ void Anim::initCL(const ge::Event<ge::InitEvent>& ev) {
             type = CL_UNSIGNED_INT16;
         else
             type = CL_UNSIGNED_INT8;
-        images.push_back(cl::Image3D(cl_ctx, CL_MEM_READ_WRITE, cl::ImageFormat(CL_R, type), sz, sz, sz, 0, 0, 0, &err));
+        images.push_back(cl::Image3D(cl_ctx, CL_MEM_READ_WRITE, cl::ImageFormat(CL_R, type), sz, sz, sz, 0, 0, 0, &cl_err));
         CL_ERR("creating histogram level failed");
         sz /= 2;
     }
 
-    char *source_code;
-    uint32 code_size;
-
-    if (!glt::readFile(engine.out(), "programs/mc/cl/program.cl", source_code, code_size)) {
-        ERR("failed reading CL program");
-        return;
-    }
-
-    cl::Program::Sources source(1, std::make_pair(source_code, code_size));
-    cl_program = cl::Program(cl_ctx, source, &err);
-    CL_ERR("creating program failed");
-
-    std::stringstream num;
-    num << N;
-    err = cl_program.build(devices, (std::string("-I programs/mc/ -DSIZE=") + num.str()).c_str());
-    engine.out() << "building cl program: " << (err == CL_SUCCESS ? "success" : "failed") << ", log:" << sys::io::endl;
-    engine.out() << cl_program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(dev) << sys::io::endl;
-
-    if (err != CL_SUCCESS)
-        return;
-
-    kernel_generateSphereVolume = cl::Kernel(cl_program, "generateSphereVolume", &err);
-    CL_ERR("creating kernel failed: generateSphereVolume");
-    kernel_computeHistogramBaseLevel = cl::Kernel(cl_program, "computeHistogramBaseLevel", &err);
-    CL_ERR("creating kernel failed: computeHistogramBaseLevel");
-
     kernel_generateSphereVolume.setArg(0, volumeData);
     kernel_generateSphereVolume.setArg(1, mat4());
-    kernel_generateSphereVolume.setArg(2, vec4(0.f));
-    kernel_generateSphereVolume.setArg(3, 1.f);
+    kernel_generateSphereVolume.setArg(2, vec4(real(N) * 0.5f));
+    kernel_generateSphereVolume.setArg(3, real(N) * 0.25f);
 
-    cl_q.enqueueNDRangeKernel(kernel_generateSphereVolume, cl::NullRange, cl::NDRange(N, N, N), cl::NullRange);
-    
-    ev.info.success = true;
+    cl_q.enqueueNDRangeKernel(
+        kernel_generateSphereVolume,
+        cl::NullRange, cl::NDRange(N, N, N), cl::NullRange);
+
+    *success = true;
+}
+
+
+size Anim::computeHistogram() {
+
+    kernel_computeHistogramBaseLevel.setArg(0, images[0]);
+    kernel_computeHistogramBaseLevel.setArg(1, volumeData);
+    kernel_computeHistogramBaseLevel.setArg(2, 128);
+
+    cl_q.enqueueNDRangeKernel(
+        kernel_computeHistogramBaseLevel,
+        cl::NullRange, cl::NDRange(N, N, N), cl::NullRange);
+
+    size sz = N;
+    defs::index i = 0;
+    while (sz > 2) {
+        sz /= 2;
+
+        kernel_computeHistogramLevel.setArg(0, images[i]);
+        kernel_computeHistogramLevel.setArg(1, images[i+1]);
+
+        cl_q.enqueueNDRangeKernel(
+            kernel_computeHistogramLevel,
+            cl::NullRange, cl::NDRange(sz, sz, sz), cl::NullRange);
+
+        ++i;
+    }
+
+    ASSERT(i + 1 == SIZE(images.size()));
+
+    int32 count[8];
+    cl::size_t<3> origin;
+    cl::size_t<3> cube;
+    origin[0] = origin[1] = origin[2] = 0;
+    cube[0] = cube[1] = cube[2] = 2;
+    cl_q.enqueueReadImage(images[images.size() - 1], CL_FALSE, origin, cube, 0, 0, count);
+    cl_q.finish();
+
+    int32 total = 0;
+    for (defs::index i = 0; i < 8; ++i)
+        total += count[i];
+
+    engine.out() << "number of triangles: " << total << sys::io::endl;
+
+    return total;
+}
+
+void Anim::constructVertices(size num_tris) {
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, num_tris * 3 * 2 * sizeof(vec3_t), 0, GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    defs::index i;
+    for (i = 0; i < SIZE(images.size()); ++i)
+        kernel_histogramTraversal.setArg(i, images[i]);
+
+    cl::BufferGL vbo_buf(cl_ctx, CL_MEM_WRITE_ONLY, vbo);
+    kernel_histogramTraversal.setArg(i, vbo_buf); ++i;
+    kernel_histogramTraversal.setArg(i, 128); ++i;
+    kernel_histogramTraversal.setArg(i, num_tris);
+
+    std::vector<cl::Memory> gl_objs;
+    gl_objs.push_back(vbo_buf);
+    cl_q.enqueueAcquireGLObjects(&gl_objs);
+
+    int global_work_size = (num_tris / 64 + 1) * 64;
+    cl_q.enqueueNDRangeKernel(
+        kernel_histogramTraversal,
+        cl::NullRange, cl::NDRange(global_work_size),
+        cl::NDRange(64));
+
+    cl_q.enqueueReleaseGLObjects(&gl_objs);
+    cl_q.finish();
+
+    glDeleteBuffers(1, &vbo);
 }
 
 void Anim::link(ge::Engine& e) {
@@ -258,7 +387,9 @@ void Anim::link(ge::Engine& e) {
 }
 
 void Anim::animate(const ge::Event<ge::AnimationEvent>&) {
-    // empty
+    size num_tris;
+    time_op(num_tris = computeHistogram());
+    time_op(constructVertices(num_tris));
 }
 
 void Anim::renderScene(const ge::Event<ge::RenderEvent>& ev) {
